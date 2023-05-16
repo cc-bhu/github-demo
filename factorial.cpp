c049cc5c22a63cbe4040ff89f68ccf1c2b7a0702
Tushar <tusharsinghmodi@gmail.com>
2023-05-16 15:56:04 +0530

    we handle negative integers

--- factorial.cpp
+++ factorial.cpp
@@ -7,6 +7,10 @@ int main() {
     cout << "Enter a positive integer: ";
     cin >> n;
 
+    if(n < 0){
+        cout << "you can't enter negative integer";
+        return -1;
+    }
     for(int i = 1; i <= n; ++i) {
         factorial *= i;
     }
