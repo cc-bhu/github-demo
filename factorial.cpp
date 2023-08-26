fd2cff4cecd63bf87eb28a05b4ba68989b417f60
Shivam Kumar Singh <shivamkumarbxr23@gmail.com>
2023-08-26 16:54:42 +0530

    fixed factorial

--- factorial.cpp
+++ factorial.cpp
@@ -6,6 +6,10 @@ int main() {
 
     cout << "Enter a positive integer: ";
     cin >> n;
+    if (n<0){
+        cout << "wrong input";
+        exit;
+    }
 
     for(int i = 1; i <= n; ++i) {
         factorial *= i;
