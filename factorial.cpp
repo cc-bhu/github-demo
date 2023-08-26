88c6ea6ddcc4a286167fb69ec58949784dab8855
Shivani <shivani857987@gmail.com>
2023-08-26 05:30:28 -0700

    refactor foct from  factorial

--- factorial.cpp
+++ factorial.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-    int n, factorial = 1;
+    int m, fact = 1;
 
     cout << "Enter a positive integer: ";
-    cin >> n;
-    if (n<0)
+    cin >> m;
+    if (m<0)
     {
         cout << "Invalid input";
         return 1;
     }
     
-    for(int i = 1; i <= n; ++i) {
-        factorial *= i;
+    for(int i = 1; i <= m; ++i) {
+        fact*= i;
     }
     
-    cout << "Factorial of " << n << " = " << factorial;
+    cout << "Factorial of " << m << " = " << fact;
 
     return 0;
 }
